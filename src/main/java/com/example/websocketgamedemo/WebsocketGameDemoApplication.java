741f012099b048efef818bd553a1d654fe1b7b5e
yangzhendong01 <yangzhendong01@58.com>
2019-09-07 10:16:09 +0800

    Initial commit

---
+++ src/main/java/com/example/websocketgamedemo/WebsocketGameDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketgamedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketGameDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketGameDemoApplication.class, args);
+    }
+
+}
